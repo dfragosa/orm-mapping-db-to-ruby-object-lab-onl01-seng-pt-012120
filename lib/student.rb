e91646315b6bd85fb07d944ec1a95134e85c90be
Derric Fragosa <fderric@gmail.com>
2020-04-06 16:22:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,8 +71,8 @@ class Student
       SELECT * FROM students
       WHERE grade == 10
       LIMIT 1;
-      binding.pry
     SQL
+    binding.pry
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end 
