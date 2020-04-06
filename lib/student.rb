a33347deaeff9c8896db91abd723ee9caea665a0
Derric Fragosa <fderric@gmail.com>
2020-04-06 16:18:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT COUNT grade FROM students
+      SELECT (COUNT grade) FROM students
       WHERE grade == 10
       LIMIT 1;
     SQL
