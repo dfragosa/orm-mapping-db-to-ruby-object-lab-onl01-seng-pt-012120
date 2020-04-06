25decb1620e459979b749d39736166a0c8912f5b
Derric Fragosa <fderric@gmail.com>
2020-04-06 16:57:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ class Student
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * FROM students 
-      WHERE grade = ?;
+      WHERE grade == ?;
     SQL
     DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db
