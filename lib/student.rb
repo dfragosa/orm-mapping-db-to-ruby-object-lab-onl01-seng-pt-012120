6519b350337ca7981d2b8dcbf3808e4c8d2f6a67
Derric Fragosa <fderric@gmail.com>
2020-04-06 15:45:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
   
   def self.first_X_students_in_grade_10(students)
     sql = <<-SQL
-      SELECT name
+      SELECT grade
       FROM students
       WHERE grade == 10;
       SQL
