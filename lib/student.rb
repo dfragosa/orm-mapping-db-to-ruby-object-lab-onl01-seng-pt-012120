5a44facb318ebe13090893722c76172e54c92cad
Derric Fragosa <fderric@gmail.com>
2020-04-06 16:32:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT id FROM students
+      SELECT * FROM students
       WHERE grade = 10
       LIMIT 1;
     SQL
