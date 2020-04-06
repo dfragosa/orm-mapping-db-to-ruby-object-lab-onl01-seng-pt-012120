bae8b8aa6bee7bd985c8b002be3daffcf2c1c1bf
Derric Fragosa <fderric@gmail.com>
2020-04-06 15:12:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
   
   def self.first_X_students_in_grade_10(students)
     sql = <<-SQL
-      SELECT COUNT (name)  FROM students
+      SELECT name COUNT (name)  FROM students
       
     SQL 
   end 
